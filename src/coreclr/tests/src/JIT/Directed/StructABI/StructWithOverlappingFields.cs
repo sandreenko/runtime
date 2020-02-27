ffb1620faad591724ee29dd689b3a35a32a92d9d
Sergey Andreenko <seandree@microsoft.com>
2020-02-26 17:03:45 -0800

    Add 2 struct return tests.
    One with cases that we want to optimize better, another with overlapping fields that was causing bugs in 2015.

---
+++ src/coreclr/tests/src/JIT/Directed/StructABI/StructWithOverlappingFields.cs
@@ -0,0 +1,129 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// This test originally showed incorrect VN for different fields with the same offset.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Opt_Error
+{    
+    [StructLayout(LayoutKind.Explicit, Pack = 1)]
+	public class FourByte
+	{
+		[FieldOffset(0)]
+		public int val;
+		[FieldOffset(0)]
+		public uint uval;
+		[FieldOffset(0)]
+		public float fval;
+		[FieldOffset(0)]
+		public byte b0;
+		[FieldOffset(1)]
+		public byte b1;
+		[FieldOffset(2)]
+		public byte b2;
+		[FieldOffset(3)]
+		public byte b3;
+
+		public FourByte()
+		{
+		}
+
+		public FourByte(int val)
+		{
+			this.val = val;
+		}
+
+		public FourByte(uint uval)
+		{
+			this.uval = uval;
+		}
+
+		public FourByte(float fval)
+		{
+			this.fval = fval;
+		}
+
+		public FourByte(byte[] array)
+		{
+			if (array.Length != 4)
+				return;
+
+			this.b0 = array[0];
+			this.b1 = array[1];
+			this.b2 = array[2];
+			this.b3 = array[3];
+		}
+
+		public byte this[int i]
+		{
+			get
+			{
+				if (i == 0) return b0;
+				else if (i == 1) return b1;
+				else if (i == 2) return b2;
+				else if (i == 3) return b3;
+				else return 0;
+			}
+
+			set
+			{
+				if (i == 0) b0 = value;
+				else if (i == 1) b1 = value;
+				else if (i == 2) b2 = value;
+				else if (i == 3) b3 = value;
+			}
+		}
+
+		public void Twiddle()
+		{
+			byte b0 = this.b0;
+			byte b1 = this.b1;
+			byte b2 = this.b2;
+			byte b3 = this.b3;
+
+			this.b0 = b3;
+			this.b1 = b2;
+			this.b2 = b1;
+			this.b3 = b0;
+		}
+
+		public byte[] ToByteArray()
+		{
+			byte[] array = new byte[4];
+
+			array[0] = b0;
+			array[1] = b1;
+			array[2] = b2;
+			array[3] = b3;
+
+			return array;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendFormat("{0:x2}-{1:x2}-{2:x2}-{3:x2}", b0, b1, b2, b3);
+			return sb.ToString();
+		}
+	}
+
+	class Program
+	{
+		private static uint old = 0;
+		static int Main(string[] args)
+		{
+			FourByte fb = new FourByte(old);
+			fb.b3 = 1;
+			uint val = fb.uval;
+			Console.WriteLine("Val " + val);
+            return 100;
+        }
+	}
+}
---
+++ src/coreclr/tests/src/JIT/Directed/StructABI/structreturn.cs
@@ -0,0 +1,286 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+class TestStructs
+{
+    struct LessNativeInt
+    {
+        public bool a;
+        public bool b;
+    }
+
+    struct NativeIntOneField
+    {
+        public long a;
+    }
+
+    struct NativeIntTwoFields
+    {
+        public int a;
+        public int b;
+    }
+
+    struct NativeIntFloatField
+    {
+        public double a;
+    }
+
+    struct NativeIntMixedFields
+    {
+        public int a;
+        public float b;
+    }
+
+
+
+
+    static LessNativeInt TestLessNativeIntReturnBlockInit()
+    {
+        LessNativeInt a = new LessNativeInt();
+        return a;
+    }
+
+    static LessNativeInt TestLessNativeIntReturnFieldInit()
+    {
+        LessNativeInt a;
+        a.a = false;
+        a.b = true;
+        return a;
+    }
+
+
+    static NativeIntOneField TestNativeIntOneFieldReturnBlockInit()
+    {
+        NativeIntOneField a = new NativeIntOneField();
+        return a;
+    }
+
+    static NativeIntOneField TestNativeIntOneFieldReturnFieldInit()
+    {
+        NativeIntOneField a;
+        a.a = 100;
+        return a;
+    }
+
+    static NativeIntTwoFields TestNativeIntTwoFieldsReturnBlockInit()
+    {
+        NativeIntTwoFields a = new NativeIntTwoFields();
+        return a;
+    }
+
+    static NativeIntTwoFields TestNativeIntTwoFieldsReturnFieldInit()
+    {
+        NativeIntTwoFields a;
+        a.a = 100;
+        a.b = 10;
+        return a;
+    }
+
+    static NativeIntFloatField TestNativeIntFloatFieldReturnBlockInit()
+    {
+        NativeIntFloatField a = new NativeIntFloatField();
+        return a;
+    }
+
+    static NativeIntFloatField TestNativeIntFloatFieldReturnFieldInit()
+    {
+        NativeIntFloatField a;
+        a.a = 100;
+        return a;
+    }
+
+    static NativeIntMixedFields TestNativeIntMixedFieldsReturnBlockInit()
+    {
+        NativeIntMixedFields a = new NativeIntMixedFields();
+        return a;
+    }
+
+    static NativeIntMixedFields TestNativeIntMixedFieldsReturnFieldInit()
+    {
+        NativeIntMixedFields a;
+        a.a = 100;
+        a.b = 10;
+        return a;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static int TestLessNativeIntCall1()
+    {
+        int res = 0;
+        var v = TestLessNativeIntReturnBlockInit();
+        if (v.a)
+        {
+            res++;
+        }
+        if (v.b)
+        {
+            res++;
+        }
+        if (v.a && v.b)
+        {
+            res++;
+        }
+        if (!v.a && !v.b)
+        {
+            res--;
+        }
+        return res;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static int TestLessNativeIntCall2()
+    {
+        int res = 0;
+        var v = TestLessNativeIntReturnFieldInit();
+        if (v.a)
+        {
+            res++;
+        }
+        if (v.b)
+        {
+            res++;
+        }
+        if (v.a && v.b)
+        {
+            res++;
+        }
+        if (!v.a && !v.b)
+        {
+            res--;
+        }
+        return res;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static int TestNativeIntOneFieldCall1()
+    {
+        int res = 0;
+        var v = TestNativeIntOneFieldReturnBlockInit();
+        if (v.a == 0)
+        {
+            res++;
+        }        
+        return res;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static int TestNativeIntOneFieldCall2()
+    {
+        int res = 0;
+        var v = TestNativeIntOneFieldReturnFieldInit();
+        if (v.a == 0)
+        {
+            res++;
+        }
+        return res;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static int TestNativeIntTwoFieldsCall1()
+    {
+        int res = 0;
+        var v = TestNativeIntTwoFieldsReturnBlockInit();
+        if (v.a == 0)
+        {
+            res++;
+        }
+        if (v.b == 0)
+        {
+            res++;
+        }
+        return res;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static int TestNativeIntTwoFieldsCall2()
+    {
+        int res = 0;
+        var v = TestNativeIntTwoFieldsReturnFieldInit();
+        if (v.a == 0)
+        {
+            res++;
+        }
+        if (v.b == 0)
+        {
+            res++;
+        }
+        return res;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static int TestNativeIntFloatFieldCall1()
+    {
+        int res = 0;
+        var v = TestNativeIntFloatFieldReturnBlockInit();
+        if (v.a >= 0)
+        {
+            res++;
+        }
+        return res;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static int TestNativeIntFloatFieldCall2()
+    {
+        int res = 0;
+        var v = TestNativeIntFloatFieldReturnFieldInit();
+        if (v.a >= 0)
+        {
+            res++;
+        }
+        return res;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static int TestNativeIntMixedFieldsCall1()
+    {
+        int res = 0;
+        var v = TestNativeIntMixedFieldsReturnBlockInit();
+        if (v.a == 0)
+        {
+            res++;
+        }
+        if (v.b == 0)
+        {
+            res++;
+        }
+        return res;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static int TestNativeIntMixedFieldsCall2()
+    {
+        int res = 0;
+        var v = TestNativeIntMixedFieldsReturnFieldInit();
+        if (v.a == 0)
+        {
+            res++;
+        }
+        if (v.b == 0)
+        {
+            res++;
+        }
+        return res;
+    }
+
+
+    public static int Main()
+    {
+        TestLessNativeIntCall1();
+        TestLessNativeIntCall2();
+        TestNativeIntOneFieldCall1();
+        TestNativeIntOneFieldCall2();
+        TestNativeIntTwoFieldsCall1();
+        TestNativeIntTwoFieldsCall2();
+        TestNativeIntFloatFieldCall1();
+        TestNativeIntFloatFieldCall2();
+        TestNativeIntMixedFieldsCall1();
+        TestNativeIntMixedFieldsCall2();
+        return 100;
+    }
+}
