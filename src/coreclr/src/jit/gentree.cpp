6db5a3640f1a85105ac1758d12a5ddc3bca61007
Sergey Andreenko <seandree@microsoft.com>
2020-04-09 18:27:29 -0700

    fix an old forgotten newline in dumps

--- src/coreclr/src/jit/gentree.cpp
+++ src/coreclr/src/jit/gentree.cpp
@@ -11290,6 +11290,8 @@ void Compiler::gtDispTree(GenTree*     tree,
                 printf(" %s", eeGetFieldName(tree->AsField()->gtFldHnd), 0);
             }
 
+            gtDispCommonEndLine(tree);
+
             if (tree->AsField()->gtFldObj && !topOnly)
             {
                 gtDispVN(tree);
