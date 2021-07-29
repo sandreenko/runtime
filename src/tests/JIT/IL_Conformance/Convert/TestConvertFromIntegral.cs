3218aabf1fd56fc8d097a64b8037d7cb593691ae
Sergey <seandree@microsoft.com>
2021-07-28 23:10:30 -0700

    update the test

--- src/tests/JIT/IL_Conformance/Convert/TestConvertFromIntegral.cs
+++ src/tests/JIT/IL_Conformance/Convert/TestConvertFromIntegral.cs
@@ -876,6 +876,15 @@ namespace TestCasts
             GenerateTest<float, ulong>(int.MinValue, sourceOp, convOvfUn, ExpectException, 0);
             GenerateTest<float, ulong>(long.MinValue, sourceOp, convOvfUn, ExpectException, 0);
             GenerateTest<float, ulong>(Single.NaN, sourceOp, convOvfUn, ExpectException, 0);
+
+            TestSimple(long.MinValue, (float)long.MinValue);
+        }
+
+        public static void TestSimple(float f, object f1)
+        {
+            Console.WriteLine("Test Simple value is " + f + ", f1 is " + f1.ToString());
+
+
         }
 
         static void TestConvertFromDouble()
