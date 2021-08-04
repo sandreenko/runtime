4f1242c7e2ab665c3e9f02e693d2f34fabcdd33d
Sergey <seandree@microsoft.com>
2021-08-03 17:35:01 -0700

    тÑnew repro out reflection.

--- src/tests/JIT/IL_Conformance/Convert/TestConvertFromIntegral.cs
+++ src/tests/JIT/IL_Conformance/Convert/TestConvertFromIntegral.cs
@@ -1,10 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-// This is conformance test for conv described in ECMA-335 Table III.8: Conversion Operations.
-// It tests int32/int64/float/double as the source and sbyte/byte/short/ushort/int/uint/long/ulong
-// as the dst.
-
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
@@ -17,36 +13,25 @@ namespace TestCasts
 {
     class Program
     {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static ulong Check(float f)
+        {
+            ulong s = checked((ulong)f);
+            return s;
+        }
+
 
         [MethodImpl(MethodImplOptions.NoInlining)]
-        static void GenerateTest(float from, OpCode fromOpcode, OpCode convOpcode, ulong expectedTo)
+        static void GenerateTest(float from)
         {
 
-            Type[] args = new Type[] { }; // No args.
-            Type returnType = typeof(ulong);
-            Console.WriteLine("Run test from " + typeof(float).FullName + ", value " + from.ToString() + " To " + typeof(ulong).FullName + "with Op " + convOpcode.Name);
-            string name = "DynamicConvertFrom" + typeof(float).FullName + "To" + typeof(ulong).FullName + from.ToString() + "Op" + convOpcode.Name;
 
-
-            Console.WriteLine("Value before, before new dynamic " + from.ToString());
-
-            DynamicMethod dm = new DynamicMethod(name, returnType, args);
-
-            Console.WriteLine("Value before, after new dynamic " + from.ToString()); 
-
-            ILGenerator generator = dm.GetILGenerator();
-
-            generator.Emit(fromOpcode, from);
-            
-            Console.WriteLine("Value before, after first emit " + from.ToString());  
-
-            generator.Emit(convOpcode);
-            generator.Emit(OpCodes.Ret);
+            Console.WriteLine("Value before, on entry " + from.ToString());
 
             try
             {
-                Console.WriteLine("Value before, in try " + from.ToString());
-                ulong res = (ulong)dm.Invoke(null, BindingFlags.Default, null, new object[] { }, null);
+               Console.WriteLine("Value before, in try " + from.ToString());
+               ulong res = Check(from); 
             }
             catch
             {
@@ -55,25 +40,16 @@ namespace TestCasts
         }
 
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        static void TestConvertFromFloat()
-        {
-            TestConvertFromFloatToU8();
-        }
 
         
         [MethodImpl(MethodImplOptions.NoInlining)]
         static void TestConvertFromFloatToU8()
         {
-            OpCode sourceOp = OpCodes.Ldc_R4;
-            OpCode convNoOvf = OpCodes.Conv_U8;
-            GenerateTest(long.MinValue, sourceOp, convNoOvf, 0);
+            GenerateTest(long.MinValue);
 
-            OpCode convOvf = OpCodes.Conv_Ovf_U8;
-            GenerateTest(long.MinValue, sourceOp, convOvf, 0);
+            GenerateTest(long.MinValue);
 
-            OpCode convOvfUn = OpCodes.Conv_Ovf_U8_Un;
-            GenerateTest(long.MinValue, sourceOp, convOvfUn, 0);
+            GenerateTest(long.MinValue);
 
 
            TestSimple(long.MinValue, (float)long.MinValue);
@@ -89,9 +65,7 @@ namespace TestCasts
 
         static int Main(string[] args)
         {
-            sbyte Zero = 0;
-            Debug.Assert(Zero.Equals(0));
-            TestConvertFromFloat();
+            TestConvertFromFloatToU8();
             return 100;
 
         }
